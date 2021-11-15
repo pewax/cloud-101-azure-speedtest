cb9ac405da081f69e50386c2e8c3e95851399c97
Peder Aalen <peaa@computas.com>
2021-11-15 17:39:02 +0100

    SpeedTestLogger

---
+++ SpeedTestLogger/LoggerConfiguration.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SpeedTestLogger
+{
+    public class LoggerConfiguration
+    {
+        public readonly RegionInfo LoggerLocation;
+        public readonly string UserId;
+        public readonly int LoggerId;
+        public LoggerConfiguration()
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var countryCode = configuration["loggerLocationCountryCode"];
+
+            UserId = configuration["userId"];
+            LoggerId = int.Parse(configuration["loggerId"]);
+            LoggerLocation = new RegionInfo(countryCode);
+
+            Console.WriteLine("Logger located in {0}", LoggerLocation.EnglishName);
+        }
+    }
+
+}
---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SpeedTest;
+using SpeedTest.Models;
+using System.Globalization;
+using System.Linq;
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SpeedTestLogger!");
+
+            var config = new LoggerConfiguration();
+            var runner = new SpeedTestRunner(config.LoggerLocation);
+            runner.RunSpeedTest();
+            var testData = runner.RunSpeedTest();
+            var results = new TestResult
+            {
+                SessionId = Guid.NewGuid(),
+                User = config.UserId,
+                Device = config.LoggerId,
+                Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Data = testData
+            };
+        }
+    }
+}
